b79729486381cc5f582f34eabff15bd4f87c32bf
Hyoki Min <raniless@naver.com>
2021-07-12 21:51:38 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("aaa");
+		System.out.println(hello.getData());
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+}
